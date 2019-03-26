e6118414ee74dbb192a0da183faa43345d29899a
icune <icune@ya.ru>
2019-03-27 01:32:42 +0300

    +Material +Header

--- main/src/app/app.module.ts
+++ main/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './com/page-part/header/header.component';
+import { MaterialModule } from './obj/material.module';
+import { MenuItemsComponent } from './com/page-part/menu-items/menu-items.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ main/src/app/obj/material.module.ts
@@ -0,0 +1,31 @@
+import {
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatTooltipModule, MatTableModule, MatSortModule, MatTabsModule, MatRadioModule
+} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+let imports = [MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatSortModule,
+    MatTabsModule,
+    MatRadioModule
+];
+
+@NgModule({
+    imports: imports,
+    exports: imports,
+})
+export class MaterialModule { }
